88e1b0a59e4d2f057185d9c53c4f0236654fefe7
Salim YOUNSI <salim.ounsi@gmail.com>
2026-07-13 18:17:21 +0100

    Modifed const PORT = 3000;

--- server.ts
+++ server.ts
@@ -11,7 +11,7 @@ const __dirname = path.dirname(__filename);
 
 async function startServer() {
   const app = express();
-  const PORT = 3000;
+  const PORT = Number(process.env.PORT) || 3000;
 
   // Middleware
   app.use(express.json());
